bc74519adc966ae6f83a0e516523454b4e66dfd3
Larissa Utermohl <larissautermohl@gmail.com>
2022-09-06 15:50:08 -0300

    exercicios

---
+++ modulo4/template-heranca-polimorfismo/build/index.js
@@ -0,0 +1,20 @@
+"use strict";
+class User {
+    constructor(id, email, name, password) {
+        console.log("Chamando o construtor da classe User");
+        this.id = id;
+        this.email = email;
+        this.name = name;
+        this.password = password;
+    }
+    getId() {
+        return this.id;
+    }
+    getEmail() {
+        return this.email;
+    }
+    getName() {
+        return this.name;
+    }
+}
+//# sourceMappingURL=index.js.map
